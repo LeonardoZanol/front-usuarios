d60b320fa3a185c339f6283d379c55624372952f
LeonardoZanol <leozianni@outlook.com>
2025-06-09 00:36:55 -0300

    feat: adicionada chamanda para api e listagem

--- src/components/ListUsers.jsx
+++ src/components/ListUsers.jsx
@@ -1,17 +1,28 @@
-import { ListItem, ListItemText, Paper } from "@mui/material"
+import { colors, Paper, Table, TableBody, TableCell, TableHead, TableRow } from "@mui/material"
 
 function ListUsers({ users }) {
 
     return (
 
-        <Paper elevation={3}>
-            <List>
-                {users.map((user) => (
-                    <ListItem key={user.id}>
-                        <ListItemText primary={user.nome} secondary={user.email} />
-                    </ListItem>
-                ))}
-            </List>
+        <Paper elevation={3} sx={{ width: "700px"}}>
+            <Table>
+                <TableHead>
+                    <TableRow>
+                        <TableCell>ID</TableCell>
+                        <TableCell>Nome</TableCell>
+                        <TableCell>E-Mail</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {users.map((user) => (
+                    <TableRow>
+                        <TableCell>{user.id}</TableCell>
+                        <TableCell>{user.nome}</TableCell>
+                        <TableCell>{user.email}</TableCell>
+                    </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
         </Paper>
 
     )
--- src/pages/Usuarios.jsx
+++ src/pages/Usuarios.jsx
@@ -1,12 +1,35 @@
 import { Container } from "@mui/material"
 import Title from "../components/Title"
 import ListUsers from "../components/ListUsers"
+import { useEffect, useState } from "react"
 
 function Usuarios() {
 
+    const [users, setUsers] = useState([])
+
+    useEffect(() => {
+
+        async function fetchUsers() {
+            try {
+                const response = await fetch("http://localhost:3000/usuarios")
+                const listUsers = await response.json()
+
+                setUsers(listUsers)
+            
+            } catch (error) {
+                console.log(error)
+            }
+        }
+
+        fetchUsers()
+
+    }, [])
+
     return (
-        <Container sx={{ display: "flex", justifyContent: "center" }}>
+        <Container sx={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }}>
             <Title text="Usuários" />
+
+            <ListUsers users={users} />
 
         </Container>
     )
