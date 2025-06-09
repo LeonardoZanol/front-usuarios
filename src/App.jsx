60a634876c476c4734c44b30d85a7ec751d42274
LeonardoZanol <leozianni@outlook.com>
2025-06-08 23:23:50 -0300

    feat: adicionada rota e componente usuarios

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Usuarios from "./pages/Usuarios"
+
 function App() {
 
   return (
-    <>
-      
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/usuarios" element={<Usuarios />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
